283348975db53d257b2eb855e7de19dcf51ac133
William Gonçalves de Oliveira <william.oliveira@matera.com>
2019-10-31 19:16:04 -0300

    Adicionando aula 31-10-2019

--- src/main/java/br/com/alura/listavip/Configuracao.java
+++ src/main/java/br/com/alura/listavip/Configuracao.java
@@ -1,13 +1,29 @@
 package br.com.alura.listavip;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class Configuracao {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Configuracao.class, args);
+	}
+
+	@Bean
+	public DataSource dataSource() {
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("SDBANCO");
+		dataSource.setPassword("SDBANCO");
+		dataSource.setUrl("jdbc:oracle:thin:@192.168.56.101:1521:bctst11g");
+		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");		
+
+		return dataSource;
 	}
 
 }
