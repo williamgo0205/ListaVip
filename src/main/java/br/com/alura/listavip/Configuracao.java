a510b4cfdd0471888f4bc61d2e54cc6c54b4759e
William Gonçalves de Oliveira <williamgo0205@gmail.com>
2019-11-15 13:55:11 -0300

    Commitando Alterações Note Will Home

--- src/main/java/br/com/alura/listavip/Configuracao.java
+++ src/main/java/br/com/alura/listavip/Configuracao.java
@@ -18,9 +18,10 @@ public class Configuracao {
 	public DataSource dataSource() {
 
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUsername("SDBANCO");
-		dataSource.setPassword("SDBANCO");
-		dataSource.setUrl("jdbc:oracle:thin:@192.168.56.101:1521:bctst11g");
+		dataSource.setUsername("sdbanco");
+		dataSource.setPassword("sdbanco");
+		//dataSource.setUrl("jdbc:oracle:thin:@192.168.56.101:1521:bctst11g");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:bctst11g");
 		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");		
 
 		return dataSource;
